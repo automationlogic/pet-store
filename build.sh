a8eaa50469085e5f79eb3f9d28d25b8c195f7ad1
root <root@ci-cd-demo.automationlogic.local>
2015-07-09 18:04:59 +0100

    More verbose when calling docker API

--- build.sh
+++ build.sh
@@ -9,14 +9,17 @@ fi
 
 mvn clean package
 #docker build -t lwilts/cicd_demo:$1 .
+echo "*** Building dockerfile ***"
 curl --request POST -H "Content-Type:application/tar" --data-binary @dockerfile http://172.17.42.1:2375/build
 #docker login -u lwilts
 #docker push lwilts/cicd_demo
 
 # LOCAL DEPLOY
 #docker kill pet-store && docker rm pet-store
+echo "*** Removing old pet-store ***"
 curl --request DELETE http://172.17.42.1:2375/containers/pet-store?force=true
 #docker run --name pet-store -p 80:8080 -d lwilts/cicd_demo:$1
+echo "*** Creating new pet-store ***"
 curl --request POST -H "Content-Type:application/json" --data-binary @container.json http://172.17.42.1:2375/containers
 
 exit 0
