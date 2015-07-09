3c730941db805b7bcfe4795e3b9761742802686e
root <root@ci-cd-demo.automationlogic.local>
2015-07-09 17:56:34 +0100

    Use docker remote API to control daemon. Ignoring docker hub push for now.

--- build.sh
+++ build.sh
@@ -8,13 +8,15 @@ then
 fi
 
 mvn clean package
-docker build -t lwilts/cicd_demo:$1 .
-docker login -u lwilts
-docker push lwilts/cicd_demo
+#docker build -t lwilts/cicd_demo:$1 .
+curl --request POST -H "Content-Type:application/tar" --data-binary @dockerfile http://172.17.42.1:2375/build
+#docker login -u lwilts
+#docker push lwilts/cicd_demo
 
 # LOCAL DEPLOY
-docker kill pet-store
-docker rm pet-store
-docker run --name pet-store -p 80:8080 -d lwilts/cicd_demo:$1 
+#docker kill pet-store && docker rm pet-store
+curl --request DELETE http://172.17.42.1:2375/containers/pet-store?force=true
+#docker run --name pet-store -p 80:8080 -d lwilts/cicd_demo:$1
+curl --request POST -H "Content-Type:application/json" --data-binary @container.json http://172.17.42.1:2375/containers
 
 exit 0
