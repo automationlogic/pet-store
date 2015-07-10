9845e9cbfaf74c26713f631fc3189823544f39d5
Luke Wiltshire <luke@luke-al.home>
2015-07-10 10:19:52 +0100

    Renamed Docker image and altered site title

--- build.sh
+++ build.sh
@@ -8,18 +8,18 @@ then
 fi
 
 mvn clean package
-#docker build -t lwilts/cicd_demo:$1 .
+#docker build -t lwilts/pet_store_demo:$1 .
 echo "*** Building dockerfile ***"
 tar zcf dockerfile.tar.gz dockerfile
-curl --verbose --request POST -H "Content-Type:application/tar" --data-binary '@dockerfile.tar.gz' http://172.17.42.1:2375/build?t=lwilts/cicd_demo:$1
+curl --verbose --request POST -H "Content-Type:application/tar" --data-binary '@dockerfile.tar.gz' http://172.17.42.1:2375/build?t=lwilts/pet_store_demo:$1
 #docker login -u lwilts
-#docker push lwilts/cicd_demo
+#docker push lwilts/pet_store_demo
 
 # REMOTE DEPLOY
 #docker kill pet-store && docker rm pet-store
 echo "*** Removing old pet-store ***"
 curl --verbose --request DELETE http://172.17.42.1:2375/containers/pet-store?force=true
-#docker run --name pet-store -p 80:8080 -d lwilts/cicd_demo:$1
+#docker run --name pet-store -p 80:8080 -d lwilts/pet_store_demo:$1
 echo "*** Creating new pet-store ***"
 sed -i "s/BUILD_NUMBER/$1/g" container.json
 curl --verbose --request POST -H "Content-Type:application/json" --data-binary '@container.json' http://172.17.42.1:2375/containers/create?name=pet-store
